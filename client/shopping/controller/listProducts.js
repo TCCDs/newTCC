777d77e7c0ec069aba76ec74b5a81d6c5964a2e0
TCCDs <61026345+TCCDs@users.noreply.github.com>
2020-05-06 22:17:45 -0300

    Atualização carrinho

--- client/shopping/controller/listProducts.js
+++ client/shopping/controller/listProducts.js
@@ -15,43 +15,60 @@ $(document).ready(function() {
                 <div class="card" style="width: 18rem;">
                 <div class="card-body">
                   <h5 class="card-title"> ` + data[i].product_name + `</h5>
-                  <h4 class="card-title text-success mt-2"><input type="text" name="quantity[]" id="quantity` + data[i].product_id + `" value="` + data[i].product_quantity + `" data-product_id="` + data[i].product_id + `" class="form-control quantity" /> </h4>
+                  <h4 class="card-title text-success mt-2"><input type="text" name="quantity[]" id="quantity` + data[i].product_id + `" value="` + data[i].product_quantity + `" data-product_id="` + data[i].product_id + `" class="form-control quantity" disabled /> </h4>
                 </div>
                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item">R$ ` + data[i].product_price + `</li>
-                  <li class="list-group-item">R$ ` + data[i].product_quantity * data[i].product_price + `
+                  <li class="list-group-item">
+                  <div class="row ">
+                  <div class="col-6 col-md-6">
+                <h6>Preço Unitário</h6></div>
+                  <div class="col-6 col-md-6"> 
+                  <h6>R$ ` + data[i].product_price + `</h6>
+                  </div>               
                   </li>
-                  <li class="list-group-item"><button name="delete" class="btn btn-danger btn-xs delete" id="` + data[i].product_id + `">Remove</button></li>
+                  <li class="list-group-item">
+                  <div class="row ">
+                  <div class="col-6 col-md-6">
+                <h6>Preço Total</h6></div>
+                  <div class="col-6 col-md-6"> 
+                  <h6>R$ ` + data[i].product_quantity * data[i].product_price + `</h6>
+                  </div>
+                  </li>
+                  <li class="list-group-item"><button name="delete" class="btn btn-danger btn-block btn-xs delete" id="` + data[i].product_id + `">Remove</button></li>
                 </ul>
-                <div class="card-body">
-                <input type="button" name="add_to_cart" id="` + data[i].ID_PRODUTOS + `" style="margin-top:5px;" class="btn btn-warning form-control add_to_cart" value="Adicionar o carrinho" />
-                </div>
               </div>
               
                     `
                 var total = total + (data[i].product_quantity * data[i].product_price);
-                $('tbody').append(listarProdutosCompra)
+                $('#cart').append(listarProdutosCompra)
 
             }
 
             //console.log(totalResult);
             let listarProdutosCompra = `
-                <tr>
-                    <td colspan="3" align="right">Total</td>
-                    <td align="right">R$ ` + total + `</td>
-                    <td></td>
-                </tr>
-
-                <tr>
-                    <td colspan="5" align="center">
-                        <form id="addHistoricoCompra">
-                            <input type="button" name="place_order" class="btn btn-warning place_order" value="Finalizar Compra" />
-                        </form>
-                    </td>
-                </tr>
+            <div class="col-12 col-md-12 saldo fixed-bottom ">
+             <div class="card text-center mt-2 ">
+                <div class="card-header">
+                <div class="row ">
+                <div class="col-6 col-md-6">
+                <small>Saldo</small>
+                <h4>20,00</h4></div>
+                <div class="col-6 col-md-6"> 
+                <small>Total</small>
+                <h4>R$ ` + total + `</h4>
+                </div>
+               </div>
+                </div>
+                <div class="card-body">
+                 <form id="addHistoricoCompra">
+                 <input type="button" name="place_order" class="btn btn-md btn-block btn-warning place_order" value="Finalizar Compra" />
+                </form>
+                </div>
+            </div>  
+            </div>           
             `
 
-            $('tbody').append(listarProdutosCompra)
+            $('#cart').append(listarProdutosCompra)
 
             // $('#order_table').html(data.order_table);
 
