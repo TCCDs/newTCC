a7acd99da5880cfa27950f0a8a63081265691611
VitorMartins28 <vitormartins2819992gmail.com>
2020-08-20 18:24:29 -0300

    payment

--- client/payment/controller/payment-coin.js
+++ client/payment/controller/payment-coin.js
@@ -10,7 +10,7 @@ $.ajax({
     success: function(dados) {
         for (var i = 0; i < dados.length; i++) {
             let produtos = `
-            
+
             <div class="row ml-2">
             <div class="mt-3 mr-2 col-12 col-md-2">
                 <div class="card" style="width: 18rem;  height: 20rem;">
@@ -26,9 +26,11 @@ $.ajax({
                 </div>
             </div>
         </div>
+
             `
             $('.produtos').append(produtos)
         }
+
         $('body').append('<script src="client/products/controller/viewProductsCustomers.js"></script>')
     }
 })
