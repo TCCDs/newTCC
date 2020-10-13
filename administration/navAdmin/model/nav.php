4c85093b607a52e2e32c84ee09d91fcc2d75899d
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-13 15:47:56 -0300

    13/10/2020'

--- administration/navAdmin/model/nav.php
+++ administration/navAdmin/model/nav.php
@@ -4,12 +4,11 @@
     include_once ('../../../server/Conn.php');
     $conn = new Conn();
 
-    //$ID_USUARIOS_ADMINISTRATION = $_SESSION['ID_USUARIOS'];
-    $ID_USUARIOS_ADMINISTRATION = 1;
+    $ID_USUARIOS_ADMINISTRATION = $_SESSION['ID_USUARIOS'];
 
-    $sql = 'SELECT * FROM administrador WHERE ID_ADMINISTRADOR = :ID_ADMINISTRADOR';
+    $sql = 'SELECT * FROM administrador WHERE ID_USUARIOS_ADMINISTRADOR = :ID_USUARIOS_ADMINISTRADOR';
     $resultado = $conn->getConn()->prepare($sql);
-    $resultado->bindParam(':ID_ADMINISTRADOR', $ID_USUARIOS_ADMINISTRATION, PDO::PARAM_INT);
+    $resultado->bindParam(':ID_USUARIOS_ADMINISTRADOR', $ID_USUARIOS_ADMINISTRATION, PDO::PARAM_INT);
     $resultado->execute();
 
     while($resultadoAdmin = $resultado->fetch(PDO::FETCH_ASSOC)) {
