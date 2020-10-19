cfaddc9a53dc35d844a6ffa61abeef17a0eb3bf4
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-19 17:16:00 -0300

    atualizacao 19/10/2020

--- recuperar/recuperarSenha.php
+++ recuperar/recuperarSenha.php
@@ -1,5 +1,6 @@
 <?php
     include_once("../server/Connect.php");
+    require_once('phpmailer/PHPMailer/class.phpmailer.php');
     $conn = new Conn();
 
     if(isset($_POST['ok'])):
@@ -22,6 +23,27 @@
 
         if ($total == 0):
             $erro[] = "O e-mail informado não existe no banco de dados.";
+        else:
+            $email 		= utf8_decode($_POST['email']);
+			$novasenha = substr(md5(time()), 0, 8);
+			$assunto 	= 'Recuperar senha';
+
+			$myEmail = "lucasgabriel@supermercadocaravelas.com.br";
+			$headers = "From: $myEmail\r\n";
+			$headers .= "Reply-To: $email\r\n";
+
+			$corpo = "Formulário enviado\n";
+			$corpo .= "Email: " . $email . "\n";
+			$corpo .= "Sua nova senha : " . $novasenha . "\n";
+
+			$email_to = $email;
+			$status = mail($email_to, $assunto, $corpo, $headers);
+
+			if ($status) {
+				echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+			} else {
+				echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+			}
         endif;
     endif;
 ?>
