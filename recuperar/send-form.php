eab7997f910207cffcbdb9493abea8602af1fc41
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-19 17:59:59 -0300

    atualizacao 19/10/2020

--- recuperar/send-form.php
+++ recuperar/send-form.php
@@ -3,31 +3,37 @@
 
 	if($_POST){
 		if(empty($_POST['email'])){
-			echo '<script>
-					$(document).ready(function(){
-						swal("Ops...","Preencha todos os campos obrigatórios!","warning");
-					});
-				</script>';
-		}else{
-			$email 		= utf8_decode($_POST['email']);
-			$novasenha = substr(md5(time()), 0, 8);
-			$assunto 	= 'Recuperar senha';
-
-			$myEmail = "lucasgabriel@supermercadocaravelas.com.br";
-			$headers = "From: $myEmail\r\n";
-			$headers .= "Reply-To: $email\r\n";
-
-			$corpo = "Formulário enviado\n";
-			$corpo .= "Email: " . $email . "\n";
-			$corpo .= "Sua nova senha : " . $novasenha . "\n";
-
-			$email_to = $email;
-			$status = mail($email_to, $assunto, $corpo, $headers);
-
-			if ($status) {
-				echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+	
+			$sql = "SELECT EMAIL_CLIENTES FROM clientes WHERE EMAIL_CLIENTES = :EMAIL_CLIENTES";
+			$resultado = $conn->getConn()->prepare($sql);
+			$resultado->bindParam(':EMAIL_CLIENTES', $email);
+			$resultado->execute();
+			$resultadoRs = $resultado->fetch(PDO::FETCH_ASSOC);
+			$total = $resultado->rowCount();
+			
+			if ($total == 0){
+				echo  "O e-mail informado não existe no banco de dados.";
 			} else {
-				echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+				$email 		= utf8_decode($_POST['email']);
+				$novasenha = substr(md5(time()), 0, 8);
+				$assunto 	= 'Recuperar senha';
+	
+				$myEmail = "lucasgabriel@supermercadocaravelas.com.br";
+				$headers = "From: $myEmail\r\n";
+				$headers .= "Reply-To: $email\r\n";
+	
+				$corpo = "Formulário enviado\n";
+				$corpo .= "Email: " . $email . "\n";
+				$corpo .= "Sua nova senha : " . $novasenha . "\n";
+	
+				$email_to = $email;
+				$status = mail($email_to, $assunto, $corpo, $headers);
+	
+				if ($status) {
+					echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+				} else {
+					echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+				}
 			}
 		}
 	}
