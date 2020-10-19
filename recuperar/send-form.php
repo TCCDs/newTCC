a997c7748beade60428cfaef0bf22c3de4f4ef75
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-19 18:35:57 -0300

    atualizacao 19/10/2020

--- recuperar/send-form.php
+++ recuperar/send-form.php
@@ -4,6 +4,8 @@
     $conn = new Conn();
 	$email = utf8_decode($_POST['email']);
 
+	if(!empty($email)){
+
 		$sql = "SELECT EMAIL_CLIENTES FROM clientes WHERE EMAIL_CLIENTES = :EMAIL_CLIENTES";
 		$resultado = $conn->getConn()->prepare($sql);
 		$resultado->bindParam(':EMAIL_CLIENTES', $email);
@@ -34,4 +36,9 @@
 				echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
 			}
 		}
+	} else {
+		echo "preencher campo email";
+	}
+
+
 
