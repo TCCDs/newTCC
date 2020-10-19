121744f859faadf8dbe6200d67945402ad6e698a
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-19 18:08:53 -0300

    atualizacao 19/10/2020

--- recuperar/send-form.php
+++ recuperar/send-form.php
@@ -1,8 +1,9 @@
 <?php
 	require_once('phpmailer/PHPMailer/class.phpmailer.php');
+	$email = utf8_decode($_POST['email']);
 
 	if($_POST){
-		if(empty($_POST['email'])){
+		if(empty($email)){
 	
 			$sql = "SELECT EMAIL_CLIENTES FROM clientes WHERE EMAIL_CLIENTES = :EMAIL_CLIENTES";
 			$resultado = $conn->getConn()->prepare($sql);
@@ -13,28 +14,26 @@
 			
 			if ($total == 0){
 				echo  "O e-mail informado não existe no banco de dados.";
-			}
-
-		}else{
-			$email 		= utf8_decode($_POST['email']);
-			$novasenha = substr(md5(time()), 0, 8);
-			$assunto 	= 'Recuperar senha';
-
-			$myEmail = "lucasgabriel@supermercadocaravelas.com.br";
-			$headers = "From: $myEmail\r\n";
-			$headers .= "Reply-To: $email\r\n";
-
-			$corpo = "Formulário enviado\n";
-			$corpo .= "Email: " . $email . "\n";
-			$corpo .= "Sua nova senha : " . $novasenha . "\n";
-
-			$email_to = $email;
-			$status = mail($email_to, $assunto, $corpo, $headers);
-
-			if ($status) {
-				echo "<script> alert('Formulário enviado com sucesso!'); </script>";
 			} else {
-				echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+				$novasenha = substr(md5(time()), 0, 8);
+				$assunto 	= 'Recuperar senha';
+	
+				$myEmail = "lucasgabriel@supermercadocaravelas.com.br";
+				$headers = "From: $myEmail\r\n";
+				$headers .= "Reply-To: $email\r\n";
+	
+				$corpo = "Formulário enviado\n";
+				$corpo .= "Email: " . $email . "\n";
+				$corpo .= "Sua nova senha : " . $novasenha . "\n";
+	
+				$email_to = $email;
+				$status = mail($email_to, $assunto, $corpo, $headers);
+	
+				if ($status) {
+					echo "<script> alert('Formulário enviado com sucesso!'); </script>";
+				} else {
+					echo "<script> alert('Falha ao enviar o Formulário.'); </script>";
+				}
 			}
 		}
 	}
