d3dd50e8b56477d98f2fc5f83bfd96238ad3ea09
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-20 10:13:27 -0300

    atualizacao 20/10/2020

--- client/receitas/app/config/global.php
+++ client/receitas/app/config/global.php
@@ -1,7 +1,7 @@
 <?php
 
-define('BASE', '/https://supermercadocaravelas.com.br/');
-define('UNSET_COUNT', 3);
+define('BASE', '/client/receitas/');
+define('UNSET_COUNT', 2);
 
 define('DB_HOST', 'mysql669.umbler.com');
 define('DB_USER', 'supercaravelas');
