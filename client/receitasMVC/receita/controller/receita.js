faaac8bbf3b55e34748b0025cdb262e5e2d88727
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-22 16:41:51 -0300

    atualizacao 22/10/2020

---
+++ client/receitasMVC/receita/controller/receita.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('.receita').empty()
+
+    var url = "client/receitasMVC/recita/model/receita.php"
+
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: url,
+        async: true,
+        success: function(dados) {
+            for (var i = 0; i < dados.length; i++) {
+                let receita = `
+                    <tr>
+                        <td> ` + dados[i].id + `</td>
+                        <td> ` + dados[i].titulo + `</td>
+                        <td> ` + dados[i].slug + `</td>
+                        <td> ` + dados[i].categoriaTitulo + `</td>
+                        <td> ` + dados[i].data|date('d/m/Y H:i:s') + `</td>
+                        <td>
+                            <a href="{{BASE}}receita/editar/` + dados[i].id + `" class="btn btn-warning">Editar</a>
+                            <a href="{{BASE}}receita/ver/` + dados[i].slug + `" class="btn btn-info ml-2">Ver</a>
+                        </td>
+                    </tr>
+                `
+                $('.receita').append(receita)
+            }
+        }
+    })
+})
---
+++ client/receitasMVC/receita/model/receita.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    include_once ('../../../../server/Connect.php');
+    $conn = new Conn();
+
+    $categoriaid = 5;
+
+    $sql = "SELECT 
+                r.*, 
+                c.titulo as cattitulo 
+            FROM receita r 
+            INNER JOIN receita_categoria c ON c.id = r.categoria_id 
+            WHERE r.categoria_id = :categoriaid 
+            ORDER BY r.data DESC
+            ";
+
+    $resultado = $conn->getConn()->prepare($sql);
+    $resultado->bindParam(':categoriaid', $categoriaid, PDO::PARAM_INT);
+    $resultado->execute();
+
+    while($resultadoReceita = $resultado->fetch(PDO::FETCH_ASSOC)) {
+        $receita[] = array_map('utf8_encode', $resultadoReceita);
+    }
+
+    echo json_encode($receita,  JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+
+
+?>
