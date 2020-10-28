f3a1c5197ceccef895db89252d1703c68f75abf9
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-10-27 21:39:44 -0300

    testr

--- recuperar/index.php
+++ recuperar/index.php
@@ -5,24 +5,21 @@
 		</div>
 	</div>
 
-		<form action="" method="post" enctype="multipart/form-data" class="form">
-
-		<div id="formCad">
-			<div class="row">
-				<div class="col-12 col-md-12">
-					<div class=" form-group form">
-						<input type="text" name="email" id="example-search-input" aria-autocomplete="off" required>
-						<label for="email" class="label-input">
-							<span class="content-input">Digite seu email</span>
-						</label>
-					</div>
+	<form action="" method="post" enctype="multipart/form-data" class="form">
+			<div class="form-group row">
+			  	<label for="example-search-input" class="col-xs-2 col-form-label">Email*</label>
+				<div class="col-xs-10">
+					<input class="form-control" name="email" type="email" value="" id="example-search-input" >
 				</div>
 			</div>
-			<button type="submit" name="ok" id="ok" class="btn btn-primary pull-left" id="btn-contato">Enviar mensagem</button>
-			<!-- <div class="loading" style="float: left;margin-left:20px;"></div> -->
-			</div>	
+			<div class="form-group row">
+				<label class="col-xs-2 col-form-label"></label>
+				<div class="col-xs-10">
+					<button type="submit" name="ok" id="ok" class="btn btn-primary pull-left" id="btn-contato">Enviar mensagem</button>
+					<div class="loading" style="float: left;margin-left:20px;"></div>
+				</div>
+			</div>
 		</form>		
-	
 		<div class="mostrar"></div>
 	
 <script>
