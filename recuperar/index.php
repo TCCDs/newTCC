b634207dcccd056e206e0c034a46a4def123cbe8
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-10-27 21:33:10 -0300

    teste r

--- recuperar/index.php
+++ recuperar/index.php
@@ -11,13 +11,14 @@
 			<div class="row">
 				<div class="col-12 col-md-12">
 					<div class=" form-group form">
+					<button type="submit">oi</button>
+
 						<input type="text" name="email" id="example-search-input" aria-autocomplete="off" required>
 						<label for="email" class="label-input">
 							<span class="content-input">Digite seu email</span>
 						</label>
 					</div>
 				</div>
-				<button type="submit">oi</button>
 			</div>
 			<button type="submit" name="ok" id="ok" class="btn btn-primary pull-left" id="btn-contato">Enviar mensagem</button>
 			<div class="loading" style="float: left;margin-left:20px;"></div>
