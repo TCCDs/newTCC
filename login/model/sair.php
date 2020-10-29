5ecdb6b4e9958916515ce00a9a2801f473160cfa
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-10-29 10:18:53 -0300

    atualizacao 29/10/2020

--- login/model/sair.php
+++ login/model/sair.php
@@ -4,4 +4,4 @@ session_start();
 unset($_SESSION['ID_USUARIOS'], $_SESSION['LOGIN_USUARIOS']);
 
 $_SESSION['msg'] = "Deslogado com sucesso";
-header("Location: ../view/index.html");
+header("Location: ../view/index.php");
