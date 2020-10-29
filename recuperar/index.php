99b26c5c82f6227515d6f7d9b6437a0de3057a9c
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-10-28 21:22:12 -0300

    recuperar

--- recuperar/index.php
+++ recuperar/index.php
@@ -1,28 +1,4 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Formulário em Ajax e Alerta Personalizado</title>
-	
-	<!-- jquery -->
-	<script  src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-	
-	<!-- bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	
-	<script src="js/sweetalert.js"></script>
-	
-</head>
-<body>
-	<div class="container">
-		
-		<br><br>
-		<h3 id="textual-inputs">Formulário em Ajax</h3>
-		<br><br>
-		
-		<form action="" method="post" enctype="multipart/form-data" class="form">
+<form action="" method="post" enctype="multipart/form-data" class="form">
 			<div class="form-group form">
 			<input type="text" name="email" id="example-search-input" aria-autocomplete="off" required>
                 <label for="example-search-input" class="label-input">
@@ -56,10 +32,3 @@
 		});
 	});
 </script>	
-	
-	
-	
-	
-	
-</body>
-</html>
