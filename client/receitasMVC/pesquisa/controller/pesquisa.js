b124a312555d582de34388aa60f29d41ca408b2c
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-11-03 16:47:58 -0300

    atualizacao 03/11/2020

--- client/receitasMVC/pesquisa/controller/pesquisa.js
+++ client/receitasMVC/pesquisa/controller/pesquisa.js
@@ -1,65 +1,35 @@
-/*$(function(){
-	//Pesquisar os cursos sem refresh na página
-	$("#pesquisa").keyup(function(){
-		var pesquisa = $(this).val();
-		//Verificar se há algo digitado
-		if(pesquisa != ''){
-			var dados = {
-				palavra : pesquisa
-			}		
-			$.post('client/receitasMVC/pesquisa/model/pesquisa.php', dados, function(retorna){
-				//Mostra dentro da ul os resultado obtidos 
-				$("tbody").html(retorna);
+$(function(){
+	$('.carregando').hide();
+	$('#pesquisa').change(function(){
+		if( $(this).val() ) {
+			$('tbody').hide();
+			$('.carregando').show();
+			$.getJSON('client/receitasMVC/pesquisa/model/pesquisa.php?search=',{pesquisa: $(this).val(), ajax: 'true'}, function(dados){
+			  var valor = '';	
+				for (var i = 0; i < dados.length; i++) {
+	  valor += `<tr>
+				<td> ` + dados[i].id + `</td>
+				<td> ` + dados[i].titulo + `</td>
+				<td> ` + dados[i].slug + `</td>
+				<td> ` + dados[i].cattitulo + `</td>
+				<td> ` + dados[i].data + `</td>
+				<td>
+					<button id="` + dados[i].id + `" class="btn btn-warning btn-receitaEditar"> 
+						Editar
+					</button>
+
+					<button id="` + dados[i].slug + `" class="btn btn-info ml-2 btn-receitaView"> 
+						Ver
+					</button>
+				</td>
+			</tr>`;
+				}	
+				$('tbody').html(valor).show();
+				$('.carregando').hide();
 			});
-		}else{
-			$("tbody").html('');
-		}		
+		} else {
+			$('.carregando').hide();
+			$('tbody').html('Nenhuma empresa encontrada');
+		}
 	});
-});*/
-
-
-$(document).ready(function(){
-
-    $('#pesquisa').keyup(function(){
-
-        $('#form-pesquisa').submit(function(){
-            var dados = $(this).serialize();
-
-            $.ajax({
-                url: 'client/receitasMVC/pesquisa/model/pesquisa.php',
-                method: 'post',
-                dataType: 'json',
-                data: dados,
-                success: function(dados){
-					for (var i = 0; i < dados.length; i++) {
-						let pesquisa = `
-							<tr>
-								<td> ` + dados[i].id + `</td>
-								<td> ` + dados[i].titulo + `</td>
-								<td> ` + dados[i].slug + `</td>
-								<td> ` + dados[i].cattitulo + `</td>
-								<td> ` + dados[i].data + `</td>
-								<td>
-									<button id="` + dados[i].id + `" class="btn btn-warning btn-receitaEditar"> 
-										Editar
-									</button>
-		
-									<button id="` + dados[i].slug + `" class="btn btn-info ml-2 btn-receitaView"> 
-										Ver
-									</button>
-								</td>
-							</tr>
-						`
-						$('tbody').append(pesquisa)
-					}
-                    //$('#resultado').empty().html(data);
-                }
-            });
-
-            return false;
-        });
-
-        $('#form-pesquisa').trigger('submit');
-
-    });
 });
--- client/receitasMVC/pesquisa/model/pesquisa.php
+++ client/receitasMVC/pesquisa/model/pesquisa.php
@@ -1,34 +1,33 @@
 <?php
-    session_start();
-    include_once ('../../../../server/Conn.php');
-    $conn = new Conn();
+	include_once("../../../../server/Conn.php");
+	$conn = new Conn();
 
-    $pesquisa = $_POST['pesquisa'];
+	$pesquisa = $_REQUEST['pesquisa'];
 
-    $resultPesquisa = array(
+	$resultPesquisa = array(
         ':titulo' => strtoupper("%{$pesquisa}%"),
         ':linha_fina' => strtoupper("%{$pesquisa}%"),
         ':descricao' => strtoupper("%{$pesquisa}%")
     );
 
-    $sql = "SELECT 
-                r.*, 
-                c.titulo as cattitulo 
-            FROM receita r 
-            INNER JOIN receita_categoria c ON c.id = r.categoria_id 
-            WHERE UPPER(r.titulo) LIKE :titulo OR 
-            UPPER(r.linha_fina) LIKE  :linha_fina OR
-            UPPER(r.descricao) LIKE :descricao
-            ORDER BY r.titulo ASC 
-            ";
+	$sql = "SELECT 
+				r.*, 
+				c.titulo as cattitulo 
+			FROM receita r 
+			INNER JOIN receita_categoria c ON c.id = r.categoria_id 
+			WHERE UPPER(r.titulo) LIKE :titulo OR 
+			UPPER(r.linha_fina) LIKE  :linha_fina OR
+			UPPER(r.descricao) LIKE :descricao
+			ORDER BY r.titulo ASC 
+		";
 
-    $resultado = $conn->getConn()->prepare($sql);
-    $resultado->execute($resultPesquisa);
+	$resultado = $conn->getConn()->prepare($sql);
+	//$resultado->bindParam(':titulo', $estado_id, PDO::PARAM_INT);
+	$resultado->execute($resultPesquisa);
 
-    while($resultadoPesquisa = $resultado->fetch(PDO::FETCH_ASSOC)) {
-        $pesquisa[] = array_map('utf8_encode', $resultadoPesquisa);
+	while($resultadoReceita = $resultado->fetch(PDO::FETCH_ASSOC)) {
+		$receita[] = array_map('utf8_encode', $resultadoReceita);
     }
+    
 
-    echo json_encode($pesquisa,  JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-
-?>
+	echo json_encode($receita,  JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
