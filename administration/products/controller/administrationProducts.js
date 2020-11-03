82e2251b1de3c8e68d57a8810c5485c510d82b2a
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-02 21:52:59 -0300

    adm

--- administration/products/controller/administrationProducts.js
+++ administration/products/controller/administrationProducts.js
@@ -16,9 +16,9 @@ $(document).ready(function() {
                     <td class="text-center" width="20%"> ` + dados[i].PRECO_VENDA_PRODUTOS + ` </td>
                     <td class="text-center" width="20%"> ` + dados[i].VALIDADE_PRODUTOS + ` </td>
                     <td class="text-center" width="15%">
-                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-outline-primary btn-sm btn-view-produtos"> <i class="mdi mdi-eye mdi-18px"></i> </button>
-                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-outline-warning btn-sm btn-edit"> <i class="mdi mdi-pencil mdi-18px"></i> </button>
-                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-outline-danger btn-sm btn-delete"> <i class="mdi mdi-delete mdi-18px"></i> </button>
+                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-primary btn-sm btn-view-produtos"> <i class="mdi mdi-eye mdi-18px"></i> </button>
+                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-warning btn-sm btn-edit"> <i class="mdi mdi-pencil mdi-18px"></i> </button>
+                        <button id="` + dados[i].ID_PRODUTOS + `" class="btn btn-danger btn-sm btn-delete"> <i class="mdi mdi-delete mdi-18px"></i> </button>
                     </td>
                 </tr>
                 `
@@ -27,7 +27,7 @@ $(document).ready(function() {
             $('body').append('<script src="administration/products/controller/viewProductsAdministration.js"></script>')
                 //$('body').append('<script src="administration/products/controller/edit-produto.js"></script>')
             $('body').append('<script src="administration/products/controller/deleteProductsAdministration.js"></script>')
-            $('body').append('<script>$(".zz").click(function(){ $("#conteudo").load("administration/form/formProdutos/view/formProducts.html")})</script>')
+            $('body').append('<script>$(".produtos-add").click(function(){ $("#conteudo").load("administration/form/formProdutos/view/formProducts.html")})</script>')
 
         }
     })
