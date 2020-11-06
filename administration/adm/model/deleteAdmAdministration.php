e9a6c961281bf72ea9179efe0f3371e286408ac9
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-11-06 09:19:43 -0300

    atualizacao 06/11/2020

--- administration/adm/model/deleteAdmAdministration.php
+++ administration/adm/model/deleteAdmAdministration.php
@@ -7,7 +7,7 @@
 
     try {
         //$sql = "DELETE FROM ADMINISTRADOR WHERE ID_ADMINISTRADOR = :ID_ADMINISTRADOR";
-        $sql = "UPDATE ADMINISTRADOR SET STATUS_ADMINISTRADOR = :STATUS_ADMINISTRADOR WHERE ID_ADMINISTRADOR = :ID_ADMINISTRADOR";
+        $sql = "UPDATE administrador SET STATUS_ADMINISTRADOR = :STATUS_ADMINISTRADOR WHERE ID_ADMINISTRADOR = :ID_ADMINISTRADOR";
         $resultado = $conn->getConn()->prepare($sql);
         $resultado->bindParam(':ID_ADMINISTRADOR', $idAdministrador);
         $resultado->bindParam(':STATUS_ADMINISTRADOR', $statusAdministrador);
