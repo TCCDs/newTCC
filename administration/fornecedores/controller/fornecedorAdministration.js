39db2bfb8f36adf30e7944bcc192e17fb736671e
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-08 18:17:06 -0300

    fornecedor

--- administration/fornecedores/controller/fornecedorAdministration.js
+++ administration/fornecedores/controller/fornecedorAdministration.js
@@ -23,14 +23,16 @@ $(document).ready(function() {
                     <div class="mt-3 mr-2 col-12 col-md-2">
                         <div class="card" style="width: 20em;  height: 23rem;">
                             <div class="card-body">
-                                    <h5 class="card-title text-dark">` + dados[i].NOME_MARCA + `</h5>
+                                    <h5 class="card-title text-dark">` + dados[i].NOME_FANTASIA_FORNECEDORES + `</h5>
                                     <ul class="list-group list-group-flush">
-                                        <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span><strong>Código da marca</strong><span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].CODIGO_MARCA + `</div></div></li>
-                                        <li class="list-group-item"><button id="` + dados[i].ID_MARCA + `" class="btn  btn-block btn-primary btn-sm btn-view-marcas"> <i class="mdi mdi-eye mdi-18px "></i> </button>
+                                    <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span><strong>Razão social</strong><span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].RAZAO_SOCIAL_FORNECEDORES + `</div></div></li>
+                                    <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span><strong>CNPJ</strong><span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].CNPJ_FORNECEDORES + `</div></div></li>
+                                    <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span><strong>Email</strong><span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].EMAIL_FORNECEDORES + `</div></div></li>
+                                        <li class="list-group-item"><button id="` + dados[i].ID_FORNECEDORES + `" class="btn  btn-block btn-primary btn-sm btn-view-marcas"> <i class="mdi mdi-eye mdi-18px "></i> </button>
                                         </li>
-                                        <li class="list-group-item"> <button id="` + dados[i].ID_MARCA + `" class="btn btn-block btn-warning btn-sm btn-edit-marcas"> <i class="mdi mdi-pencil mdi-18px"></i> </button>
+                                        <li class="list-group-item"> <button id="` + dados[i].ID_FORNECEDORES + `" class="btn btn-block btn-warning btn-sm btn-edit-marcas"> <i class="mdi mdi-pencil mdi-18px"></i> </button>
                                         </li>
-                                        <li class="list-group-item"> <button id="` + dados[i].ID_MARCA + `" class="btn btn-block btn-danger btn-sm btn-delete"> <i class="mdi mdi-delete mdi-18px"></i> </button>
+                                        <li class="list-group-item"> <button id="` + dados[i].ID_FORNECEDORES + `" class="btn btn-block btn-danger btn-sm btn-delete"> <i class="mdi mdi-delete mdi-18px"></i> </button>
                                         </li>
                                     </ul>
                             </div>
