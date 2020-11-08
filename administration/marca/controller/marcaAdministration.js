08dff3d3e1018254b72cf79d58e36ab30cbdd72a
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-08 17:38:22 -0300

    marca

--- administration/marca/controller/marcaAdministration.js
+++ administration/marca/controller/marcaAdministration.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
                 let marca = `
                 <div class="row ml-2">
                     <div class="mt-3 mr-2 col-12 col-md-2">
-                        <div class="card" style="width: 20em;  height: 25rem;">
+                        <div class="card" style="width: 20em;  height: 23rem;">
                             <div class="card-body">
                                     <h5 class="card-title text-dark">` + dados[i].NOME_MARCA + `</h5>
                                     <ul class="list-group list-group-flush">
