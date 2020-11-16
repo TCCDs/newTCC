de3145b96afe9a40d491100ef44fff94cc2bb6ab
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-16 20:15:59 -0300

    receita

--- client/receitasMVC/categoria/controller/controller.js
+++ client/receitasMVC/categoria/controller/controller.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.btn-categoriaEditar').click(function() {
-        $('body').load('client/receitasMVC/categoria/view/editar.html')
+        $('#conteudo').load('client/receitasMVC/categoria/view/editar.html')
     })
 
 })
