01675c491a0065b112eee8a4d7a7605cd32d06ea
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-20 16:40:39 -0300

    receita

--- client/receitasMVC/receita/controller/receita.js
+++ client/receitasMVC/receita/controller/receita.js
@@ -17,9 +17,7 @@ $(document).ready(function() {
                         <td> ` + dados[i].cattitulo + `</td>
                         <td> ` + dados[i].data + `</td>
                         <td>
-                            <button id="` + dados[i].id + `" class="btn btn-receitaEditar"> 
-                                Editar
-                            </button>
+                        <button id="` + dados[i].id + `" class="btn  btn-block btn-primary btn-sm .btn-receitaEditar> <i class="mdi mdi-eye mdi-18px "></i> </button>
 
                             <button id="` + dados[i].id + `" class="btn btn-info ml-2 btn-receitaView"> 
                                 Ver
