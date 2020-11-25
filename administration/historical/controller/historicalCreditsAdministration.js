5ba5672e61e402dc6d4705bb8d66b6f261f4c205
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-11-25 20:37:00 -0300

    atualizacao 25/11/2020

--- administration/historical/controller/historicalCreditsAdministration.js
+++ administration/historical/controller/historicalCreditsAdministration.js
@@ -15,12 +15,11 @@ $(document).ready(function() {
                 /*var confMoedas = {
                     style: "currency",
                     currency: "BRL"
-                }
+                }*/
 
-                var resultValorMoedas = valorMoedas.toLocaleString('pt-BR', confMoedas)
-
-                console.log(resultValorMoedas)*/
+                
                var resultValorMoedas = parseFloat(valorMoedas).toFixed(2).replace('.', ',')
+               //var teste = resultValorMoedas.toLocaleString('pt-BR', confMoedas)
 
                 let historicoMoedas = `
                 <div class="row ml-2">
@@ -29,7 +28,7 @@ $(document).ready(function() {
                         <div class="card-body">
                                 <h5 class="card-title text-dark">` + dados[i].NOME_CLIENTES + `</h5>
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item">` + resultValorMoedas + `</li>
+                                    <li class="list-group-item">R$: ` + resultValorMoedas + `</li>
                                     <li class="list-group-item"> <button id="` + dados[i].ID_MOEDAS + `" class="btn btn-outline-primary btn-block btn-sm btn-view-credito"> <i class="mdi mdi-eye mdi-18px"></i> </button>
                                     </li>
                                 </ul>
