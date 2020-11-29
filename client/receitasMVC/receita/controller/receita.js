eb0e063a57cdcb7f3ac58a31ae511e6ad3b818ee
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-11-29 14:45:45 -0300

    receita

--- client/receitasMVC/receita/controller/receita.js
+++ client/receitasMVC/receita/controller/receita.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
                         <td> ` + dados[i].cattitulo + `</td>
                         <td> ` + dados[i].data + `</td>
                         <td>
-                            <button " class="btn btn-receitaEditar" data-toggle="modal" data-target="modalReceita"> 
+                            <button " class="btn" data-toggle="modal" data-target="modalReceita"> 
                                 Editar
                             </button>
 
