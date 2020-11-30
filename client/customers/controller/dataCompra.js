6185077d0fab60dca3f303f804aa8bdd88c27f88
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-11-30 08:54:58 -0300

    atualizacao 30/11/2020

--- client/customers/controller/dataCompra.js
+++ client/customers/controller/dataCompra.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 
     var url = "client/customers/model/dataCompra.php"
 
-        function adicionaZero(numero) {
+    /*function adicionaZero(numero) {
         if (numero <= 9)
             return "0" + numero;
         else
             return numero;
-    }
+    }*/
 
     $.ajax({
         type: 'POST',
@@ -17,12 +17,11 @@ $(document).ready(function() {
         async: true,
         success: function(dados) {
             for (var i = 0; i < dados.length; i++) {
-                let dataAtual2 = new Date(dados[i].DATA_CAD);
-                let dataAtualFormatada2 = (adicionaZero(dataAtual2.getDate().toString()) + "/" + (adicionaZero(dataAtual2.getMonth() + 1).toString()) + "/" + dataAtual2.getFullYear());
+                // let dataAtual2 = new Date(dados[i].DATA_CAD);
+                // let dataAtualFormatada2 = (adicionaZero(dataAtual2.getDate().toString()) + "/" + (adicionaZero(dataAtual2.getMonth() + 1).toString()) + "/" + dataAtual2.getFullYear());
 
                 let dataCompra = `
-
-                <h6 class="text-left dataCompra-cliente">` + dataAtualFormatada2 + `</h6>
+                    <h6 class="text-left dataCompra-cliente">` + dados[i].DATA_CAD + `</h6>
                 `
                 $('.dataCompra-cliente').append(dataCompra)
             }
