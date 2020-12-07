ca09b5358e0e14c9179618cdcda944fe2a0fe689
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-07 10:57:38 -0300

    atualizacao 07/12/2020

--- client/historical/purchases/model/viewComprasDados.php
+++ client/historical/purchases/model/viewComprasDados.php
@@ -3,42 +3,28 @@
     include_once ('../../../../server/Connect.php');
     $conn = new Conn();
 
-    $idDadosCompras = 1; //$_POST['ID_USUARIOS'];
-
-    print_r($idDadosCompras);
+    $idDadosCompras = 1018892394; //$_POST['ID_USUARIOS'];
 
     $sql = 'SELECT 
-                compras_pagamentos.NOME_CARTAO,
-                compras_pagamentos.NUMERO_CARTAO,
-                compras_pagamentos.STATUS_PAGAMENTO,
-                compras_pagamentos.CODIGO_PAGAMENTO,
-                
                 compras.CODIGO_COMPRAS,
                 compras.VALOR_COMPRAS,
                 compras.STATUS_COMPRAS,
                 compras.DATA_CAD_COMPRAS,
                 
                 clientes.NOME_CLIENTES,
-                clientes.CEP_CLIENTES,
-                clientes.CIDADE_CLIENTES,
-                clientes.ESTADO_CLIENTES,
-                clientes.ENDERECO_CLIENTES,
-                clientes.NUMERO_CLIENTES,
-                clientes.BAIRRO_CLIENTES,
-
+                
                 compras_itens.NOME_PRODUTOS,
                 compras_itens.CODIGO_ITENS
-            FROM 
-                compras_pagamentos
-            INNER JOIN compras_itens ON compras_pagamentos.ID_COMPRAS_ITENS = compras_itens.ID_COMPRA_ITENS
-            INNER JOIN compras ON compras_itens.ID_COMPRAS = compras.ID_COMPRAS
-            INNER JOIN clientes ON (compras.ID_CLIENTES_COMPRAS = clientes.ID_CLIENTES)
-            WHERE 
-                clientes.ID_USUARIOS = :ID_USUARIOS
-            ORDER BY compras_itens.NOME_PRODUTOS DESC';
+                FROM 
+                compras
+                INNER JOIN compras_itens ON compras.ID_COMPRAS = compras_itens.ID_COMPRA_ITENS
+                INNER JOIN clientes ON (compras.ID_CLIENTES_COMPRAS = clientes.ID_CLIENTES)
+                WHERE 
+                    compras.CODIGO_COMPRAS = :CODIGO_COMPRAS
+                ORDER BY compras_itens.NOME_PRODUTOS DESC';
 
     $resultado = $conn->getConn()->prepare($sql);
-    $resultado->bindParam(':ID_USUARIOS', $idDadosCompras);
+    $resultado->bindParam(':CODIGO_COMPRAS', $idDadosCompras);
     $resultado->execute();
 
     while($resultadoCompras = $resultado->fetch(PDO::FETCH_ASSOC)) {
