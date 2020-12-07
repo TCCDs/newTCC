022668dc0af669d5d6336064c69e2c7b27a7951f
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-07 19:45:57 -0300

    atualizacao 07/12/2020

--- customerPanel.php
+++ customerPanel.php
@@ -1,5 +1,4 @@
 <?php
-/*
     session_start();
 
     if(!empty($_SESSION['ID_USUARIOS'])):
@@ -7,7 +6,6 @@
     else:
         header("Location: ./index.php");	
     endif;
-    */
 ?>
 
 <!DOCTYPE html>
