2d8438383bb2d6d9d170a6e96e4227f4df02ba95
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-07 09:40:42 -0300

    atualizacao 07/12/2020

--- client/historical/purchases/controller/detalhesCliente.js
+++ client/historical/purchases/controller/detalhesCliente.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
 
     var url = "client/historical/purchases/model/detalhesComprasDados.php"
 
-
-
     $.ajax({
         type: 'POST',
         dataType: 'json',
@@ -12,13 +10,11 @@ $(document).ready(function() {
         async: true,
         success: function(dados) {
             for (var i = 0; i < dados.length; i++) {
-
-
                 let detalhesDadosCompras = `
                 <div class="row">
-                <div class="mt-3 ml-2 mr-2 col-12 col-sm-6 col-md-4">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
+                    <div class="mt-3 ml-2 mr-2 col-12 col-sm-6 col-md-4">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
                                 <h5 class="card-title text-dark">Dados do cliente</h5>
                                 <ul class="list-group list-group-flush">
                                     <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span>Nome do cliente<span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].NOME_CLIENTES + `</div></div></li>
@@ -29,10 +25,10 @@ $(document).ready(function() {
                                     <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span>Número<span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].NUMERO_CLIENTES + `</div></div></li>
                                     <li class="list-group-item"><div class="row mt-2"><div class="col-12 col-md-12"><span>Bairro<span/></div><div class="col-12 col-md-12 mt-2">` + dados[i].BAIRRO_CLIENTES + `</div></div></li>
                                 </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
                 `
                 $('.listaClientes').append(detalhesDadosCompras)
             }
