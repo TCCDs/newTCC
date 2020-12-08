c7991e2bd8e003238f1ec67aac0f6a4b57028218
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-12-07 21:04:59 -0300

    teste

--- client/leitorQr/controller/qrcode.js
+++ client/leitorQr/controller/qrcode.js
@@ -5,32 +5,32 @@ $(document).ready(function() {
         var dados = $('#form-qrcode').serialize()
         var url = "client/leitorQr/model/leitorProdutos.php"
 
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            url: url,
-            async: true,
-            data: dados,
-            success: function(dados) {
-                if (dados.return == true) {
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Supermercado Caravelas!',
-                        text: 'Cadastro efetuado com sucesso!',
-                        type: 'success',
-                        confirmButtonText: 'Feito!'
-                    })
-                } else {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'SysAgenda!',
-                        text: dados.return,
-                        type: 'error',
-                        confirmButtonText: 'Tente novamente...!'
-                    })
-                }
-                $('#form-qrcode input').val("")
-            }
-        })
+        // $.ajax({
+        //     type: 'POST',
+        //     dataType: 'json',
+        //     url: url,
+        //     async: true,
+        //     data: dados,
+        //     success: function(dados) {
+        //         if (dados.return == true) {
+        //             Swal.fire({
+        //                 icon: 'success',
+        //                 title: 'Supermercado Caravelas!',
+        //                 text: 'Cadastro efetuado com sucesso!',
+        //                 type: 'success',
+        //                 confirmButtonText: 'Feito!'
+        //             })
+        //         } else {
+        //             Swal.fire({
+        //                 icon: 'error',
+        //                 title: 'SysAgenda!',
+        //                 text: dados.return,
+        //                 type: 'error',
+        //                 confirmButtonText: 'Tente novamente...!'
+        //             })
+        //         }
+        //         $('#form-qrcode input').val("")
+        //     }
+        // })
     })
 })
