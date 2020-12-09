65f4b72463a809d3f8b2eafa61d336a04a428d86
TCCDs <tccdesenvolvimento2020@gmail.com>
2020-12-09 19:55:09 -0300

    teste

--- client/leitorQr/model/leitorProdutos.php
+++ client/leitorQr/model/leitorProdutos.php
@@ -4,8 +4,7 @@
     $conn = new Conn();
 
     $qr_code = $_POST['qrcode'];
-echo 'qrcode='.$qr_code;
-exit;
+
     $sql = "SELECT  
                 ofertas.ID_PRODUTOS,
                 produtos.NOME_PRODUTOS,
