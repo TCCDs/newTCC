ecea88b62933c3fd8ad74075d18ae3fff8483c15
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-10 10:28:15 -0300

    atualizacao 10/12/2020

--- client/customers/model/graficoCompra.php
+++ client/customers/model/graficoCompra.php
@@ -3,6 +3,8 @@ header('Content-Type: application/json');
 include_once ('../../../server/Connect.php');
 $conn = new Conn();
 
+$ID_USUARIOS = $_SESSION['ID_USUARIOS'];
+
 $sql = "SELECT 
 			COUNT(compras.ID_COMPRAS) AS  total_vendas, 
 			DATE_FORMAT(compras.DATA_CAD_COMPRAS, '%m/%Y') AS  data_ ,
@@ -11,11 +13,12 @@ $sql = "SELECT
 			compras
 		INNER JOIN clientes ON compras.ID_CLIENTES_COMPRAS = clientes.ID_CLIENTES
 		WHERE 
-		DATE_FORMAT(`DATA_CAD_COMPRAS`, '%Y-%m') BETWEEN  '2020-01' AND '2020-12' AND clientes.ID_USUARIOS = 1
+		DATE_FORMAT(`DATA_CAD_COMPRAS`, '%Y-%m') BETWEEN  '2020-01' AND '2020-12' AND clientes.ID_USUARIOS = :ID_USUARIOS
 		GROUP BY data_
 		ORDER BY data_";
 
 $resultado = $conn->getConn()->prepare($sql);
+$resultado->bindParam(':ID_USUARIOS', $ID_USUARIOS);
 $resultado->execute();
 
 $data = array();
--- client/customers/model/graficoGasto.php
+++ client/customers/model/graficoGasto.php
@@ -2,6 +2,7 @@
 header('Content-Type: application/json');
 include_once ('../../../server/Connect.php');
 $conn = new Conn();
+$ID_USUARIOS = $_SESSION['ID_USUARIOS'];
 
 $sql = "SELECT 
 			sum(compras.VALOR_COMPRAS) AS  total_gastos, 
@@ -11,11 +12,12 @@ $sql = "SELECT
 			compras
 		INNER JOIN clientes ON compras.ID_CLIENTES_COMPRAS = clientes.ID_CLIENTES
 		WHERE 
-		DATE_FORMAT(`DATA_CAD_COMPRAS`, '%Y-%m') BETWEEN  '2020-01' AND '2020-12' AND clientes.ID_USUARIOS = 1
+		DATE_FORMAT(`DATA_CAD_COMPRAS`, '%Y-%m') BETWEEN  '2020-01' AND '2020-12' AND clientes.ID_USUARIOS = :ID_USUARIOS
 		GROUP BY data_
 		ORDER BY data_";
 
 $resultado = $conn->getConn()->prepare($sql);
+$resultado->bindParam(':ID_USUARIOS', $ID_USUARIOS);
 $resultado->execute();
 
 
