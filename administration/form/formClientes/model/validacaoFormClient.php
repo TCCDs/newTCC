eb9dbb3cade7c9123dcd25792f442bceec9e976a
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-11 19:13:40 -0300

    atualizacao 11/12/2020

--- administration/form/formClientes/model/validacaoFormClient.php
+++ administration/form/formClientes/model/validacaoFormClient.php
@@ -292,9 +292,9 @@
     
             $resultado = $conn->getConn()->prepare($sql);
             $resultado->execute($user_data);
-            $idCliente = $conn->getConn()->lastInsertId();
+            /*$idCliente = $conn->getConn()->lastInsertId();
 
-            $_SESSION['ID_CLIENTE'] = $idCliente;
+            $_SESSION['ID_CLIENTE'] = $idCliente;*/
     
             $mensagem = "Cadastro efetuado com sucesso!";
 
