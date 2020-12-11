4d74a050349659c9d1a6209ad9bb7fc2ccb90f8f
LucasGabriel623 <gabrieloliveira.uru@gmail.com>
2020-12-11 14:13:13 -0300

    atualizacao 11/12/2020

--- client/payment/model/descontaSaldo.php
+++ client/payment/model/descontaSaldo.php
@@ -3,6 +3,8 @@
     include_once ('../../../server/Connect.php');
     $conn = new Conn();
 
+    $erro = false;
+
     /* DESCONTO SALDO MOEDA VIRTUAL */
     $ID_USUARIOS = $_SESSION['ID_USUARIOS'];
     $sql = 'SELECT sum(saldo_clientes.SALDO_CLIENTES) AS saldo_clientes,
@@ -29,7 +31,8 @@
     }
     
     if ($totalCompraCliente > $saldoAtualCliente) {
-        echo "saldo insuficiente";
+        $erro = true;
+        $mensagem =  "saldo insuficiente"; //echo "saldo insuficiente";
     } else {
         $descontoSaldoCliente = $saldoAtualCliente - $totalCompraCliente;
     }
@@ -43,79 +46,83 @@
     $status_compras = 'F';
     $tipo_pagamento = 'Moeda Virtual';
             
-    try {
-        $sql =  "
-            INSERT INTO compras
-                (ID_CLIENTES_COMPRAS, CODIGO_COMPRAS, VALOR_COMPRAS, TOTAL_DESCONTO_COMPRAS, STATUS_COMPRAS, TIPO_PAGAMENTO) 
-            VALUES 
-                (:ID_CLIENTES_COMPRAS, :CODIGO_COMPRAS, :VALOR_COMPRAS, :TOTAL_DESCONTO_COMPRAS, :STATUS_COMPRAS, :TIPO_PAGAMENTO)
-        ";
-                
-        $resultado = $conn->getConn()->prepare($sql);
-        $resultado->bindParam(':ID_CLIENTES_COMPRAS', $ID_USUARIOS);
-        $resultado->bindParam(':CODIGO_COMPRAS', $_SESSION["CODIGO_COMPRAS"]);
-        $resultado->bindParam(':VALOR_COMPRAS', $totalCompraCliente);
-        $resultado->bindParam(':TOTAL_DESCONTO_COMPRAS', $total_desconto_compras);
-        $resultado->bindParam(':STATUS_COMPRAS', $status_compras);
-        $resultado->bindParam(':TIPO_PAGAMENTO', $tipo_pagamento);
-        $resultado->execute();
-        $id_compras = $conn->getConn()->lastInsertId();
-
-        $data = array('return' => true);
-
-    }catch (Exception $ex){
-        $data = array('return' => $ex->getMessage());
-    }
-    
-
-    #itens da compras 
-    $qr_code = mt_rand();
-    $codigo_itens = mt_rand();
-
-
-    $_SESSION["CODIGO_ITENS"] = $codigo_itens;
-    $_SESSION["QR_CODE"] = $qr_code;
-    
-    foreach($_SESSION["shopping_cart"] as $keys => $values) {
+    if (!$erro):
         try {
-            $query = "
-            INSERT INTO compras_itens
-                (CODIGO_ITENS, NOME_PRODUTOS, QUANTIDADE_PRODUTOS, PRECO_PRODUTOS, QR_CODE, ID_COMPRAS) 
-            VALUES 
-                (:CODIGO_ITENS, :NOME_PRODUTOS, :QUANTIDADE_PRODUTOS, :PRECO_PRODUTOS, :QR_CODE, :ID_COMPRAS)
-        ";
-    
-        $resultado = $conn->getConn()->prepare($query);
-        $resultado->bindParam(':CODIGO_ITENS', $_SESSION["CODIGO_ITENS"]);
-        $resultado->bindParam(':NOME_PRODUTOS', $values["product_name"]);
-        $resultado->bindParam(':QUANTIDADE_PRODUTOS', $values["product_quantity"]);
-        $resultado->bindParam(':PRECO_PRODUTOS', $values["product_price"]);
-        $resultado->bindParam(':QR_CODE', $_SESSION["QR_CODE"]);
-        $resultado->bindParam(':ID_COMPRAS', 	$id_compras);
-        $resultado->execute();
+            $sql =  "
+                INSERT INTO compras
+                    (ID_CLIENTES_COMPRAS, CODIGO_COMPRAS, VALOR_COMPRAS, TOTAL_DESCONTO_COMPRAS, STATUS_COMPRAS, TIPO_PAGAMENTO) 
+                VALUES 
+                    (:ID_CLIENTES_COMPRAS, :CODIGO_COMPRAS, :VALOR_COMPRAS, :TOTAL_DESCONTO_COMPRAS, :STATUS_COMPRAS, :TIPO_PAGAMENTO)
+            ";
+                    
+            $resultado = $conn->getConn()->prepare($sql);
+            $resultado->bindParam(':ID_CLIENTES_COMPRAS', $ID_USUARIOS);
+            $resultado->bindParam(':CODIGO_COMPRAS', $_SESSION["CODIGO_COMPRAS"]);
+            $resultado->bindParam(':VALOR_COMPRAS', $totalCompraCliente);
+            $resultado->bindParam(':TOTAL_DESCONTO_COMPRAS', $total_desconto_compras);
+            $resultado->bindParam(':STATUS_COMPRAS', $status_compras);
+            $resultado->bindParam(':TIPO_PAGAMENTO', $tipo_pagamento);
+            $resultado->execute();
+            $id_compras = $conn->getConn()->lastInsertId();
 
-        $data = array('return' => true);
+            $data = array('return' => true);
+
+        }catch (Exception $ex){
+            $data = array('return' => $ex->getMessage());
+        }
+    
+
+        #itens da compras 
+        $qr_code = mt_rand();
+        $codigo_itens = mt_rand();
+
+
+        $_SESSION["CODIGO_ITENS"] = $codigo_itens;
+        $_SESSION["QR_CODE"] = $qr_code;
+        
+        foreach($_SESSION["shopping_cart"] as $keys => $values) {
+            try {
+                $query = "
+                INSERT INTO compras_itens
+                    (CODIGO_ITENS, NOME_PRODUTOS, QUANTIDADE_PRODUTOS, PRECO_PRODUTOS, QR_CODE, ID_COMPRAS) 
+                VALUES 
+                    (:CODIGO_ITENS, :NOME_PRODUTOS, :QUANTIDADE_PRODUTOS, :PRECO_PRODUTOS, :QR_CODE, :ID_COMPRAS)
+            ";
+        
+            $resultado = $conn->getConn()->prepare($query);
+            $resultado->bindParam(':CODIGO_ITENS', $_SESSION["CODIGO_ITENS"]);
+            $resultado->bindParam(':NOME_PRODUTOS', $values["product_name"]);
+            $resultado->bindParam(':QUANTIDADE_PRODUTOS', $values["product_quantity"]);
+            $resultado->bindParam(':PRECO_PRODUTOS', $values["product_price"]);
+            $resultado->bindParam(':QR_CODE', $_SESSION["QR_CODE"]);
+            $resultado->bindParam(':ID_COMPRAS', 	$id_compras);
+            $resultado->execute();
+
+            $data = array('return' => true);
+
+            } catch (Exception $ex){
+                $data = array('return' => $ex->getMessage());
+            }
+        }
+
+        /* UPDATE ATUALIZAR SALDO */
+        try {
+            $sql = "UPDATE saldo_clientes SET SALDO_CLIENTES = :SALDO_CLIENTES  WHERE ID_CLIENTE = :ID_CLIENTE";
+
+            $resultado = $conn->getConn()->prepare($sql);
+            $resultado->bindParam(':SALDO_CLIENTES', $descontoSaldoCliente);
+            $resultado->bindParam(':ID_CLIENTE', $ID_USUARIOS);
+            $resultado->execute();
+                
+            $data = array('return' => true);
 
         } catch (Exception $ex){
             $data = array('return' => $ex->getMessage());
         }
-    }
 
-    /* UPDATE ATUALIZAR SALDO */
-    try {
-        $sql = "UPDATE saldo_clientes SET SALDO_CLIENTES = :SALDO_CLIENTES  WHERE ID_CLIENTE = :ID_CLIENTE";
+        unset($_SESSION["shopping_cart"]);
+        echo json_encode($data);
+    endif;
 
-        $resultado = $conn->getConn()->prepare($sql);
-        $resultado->bindParam(':SALDO_CLIENTES', $descontoSaldoCliente);
-        $resultado->bindParam(':ID_CLIENTE', $ID_USUARIOS);
-        $resultado->execute();
-            
-        $data = array('return' => true);
 
-    } catch (Exception $ex){
-        $data = array('return' => $ex->getMessage());
-    }
-    
-    unset($_SESSION["shopping_cart"]);
-    echo json_encode($data);
 ?>
